45a4809602919b38acbb7571e6ce0014e77b871f
jqueijo- <jqueijo-@pop-os.localdomain>
2024-01-09 16:28:10 +0000

    still useless

--- pipex.h
+++ pipex.h
@@ -6,13 +6,16 @@
 /*   By: jqueijo- <jqueijo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 11:20:13 by jqueijo-          #+#    #+#             */
-/*   Updated: 2024/01/08 12:17:23 by jqueijo-         ###   ########.fr       */
+/*   Updated: 2024/01/09 12:55:52 by jqueijo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
+# include <unistd.h> // System calls
+# include <stdlib.h> // Memory allocation
 # include <errno.h> // Error handling
+# include <sys/wait.h> // Wait and waitpid function
 
 #endif
